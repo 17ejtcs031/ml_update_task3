6ad9608e9d7e354a1c16486cb037d03681b8f181
Himani Agarwal <52791723+17ejtcs031@users.noreply.github.com>
2020-06-02 23:30:37 +0530

    beautify

--- model.py
+++ model.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from keras.applications import VGG16
-model = VGG16(weights='/root/model/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5', include_top=False, input_shape=(64, 64, 3))
+model = \
+    VGG16(weights='/root/model/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+          , include_top=False, input_shape=(64, 64, 3))
 
-model.layers[0].input        
+model.layers[0].input
 model.output
 top_model = model.output
 
@@ -13,10 +17,12 @@ from keras.models import Sequential
 from keras.layers import Dense, Flatten, Activation
 
 top_model = Flatten()(top_model)
-n1=300
-n2=200
+n1 = 300
+n2 = 200
 top_model = Dense(n1, activation='relu')(top_model)
-#addlayerhere
+
+# addlayerhere
+
 top_model = Dense(n2, activation='relu')(top_model)
 top_model = Dense(10, activation='softmax')(top_model)
 
@@ -34,87 +40,87 @@ train_data_dir = '/root/model/monkey_breed/train/'
 validation_data_dir = '/root/model/monkey_breed/validation/'
 
 train_datagen = ImageDataGenerator(
-rescale=1./255,
-rotation_range=20,
-width_shift_range=0.2,
-height_shift_range=0.2,
-horizontal_flip=True,
-fill_mode='nearest')
+    rescale=1. / 255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest',
+    )
 
-validation_datagen = ImageDataGenerator(rescale=1./255)
+validation_datagen = ImageDataGenerator(rescale=1. / 255)
 
 # Change the batchsize according to your system RAM
-train_batchsize=16
-val_batchsize=10
 
-train_generator = train_datagen.flow_from_directory(
-train_data_dir,
-target_size=(64, 64),
-batch_size=train_batchsize,
-class_mode='categorical')
+train_batchsize = 16
+val_batchsize = 10
 
-validation_generator = validation_datagen.flow_from_directory(
-validation_data_dir,
-target_size=(64, 64),
-batch_size=val_batchsize,
-class_mode='categorical',
-shuffle=False)
+train_generator = train_datagen.flow_from_directory(train_data_dir,
+        target_size=(64, 64), batch_size=train_batchsize,
+        class_mode='categorical')
+
+validation_generator = \
+    validation_datagen.flow_from_directory(validation_data_dir,
+        target_size=(64, 64), batch_size=val_batchsize,
+        class_mode='categorical', shuffle=False)
 
 from keras.optimizers import RMSprop
 
-newmodel.compile(loss = 'categorical_crossentropy',
-optimizer = RMSprop(lr = 0.001),
-metrics = ['accuracy'])
+newmodel.compile(loss='categorical_crossentropy',
+                 optimizer=RMSprop(lr=0.001), metrics=['accuracy'])
 
-nb_train_samples=1097
-nb_validation_samples=272
-epochs_x=3
-batch_size_x=40
+nb_train_samples = 1097
+nb_validation_samples = 272
+epochs_x = 3
+batch_size_x = 40
 
 import sys
-orig_stdout=sys.stdout
-f=open("/root/model/output.txt",'w')
-sys.stdout=f
+orig_stdout = sys.stdout
+f = open('/root/model/output.txt', 'w')
+sys.stdout = f
 
-history = newmodel.fit_generator(
-train_generator,
-steps_per_epoch = nb_train_samples // batch_size_x,
-epochs = epochs_x,
-validation_data = validation_generator,
-validation_steps = nb_validation_samples // batch_size_x)
+history = newmodel.fit_generator(train_generator,
+                                 steps_per_epoch=nb_train_samples
+                                 // batch_size_x, epochs=epochs_x,
+                                 validation_data=validation_generator,
+                                 validation_steps=nb_validation_samples
+                                 // batch_size_x)
 
-sys.stdout=orig_stdout
+sys.stdout = orig_stdout
 f.close()
 
+# saving accuracy in the file
 
-#saving accuracy in the file
 listoflines = list()
-with open('/root/model/output.txt','r') as myfile:
+with open('/root/model/output.txt', 'r') as myfile:
     for line in myfile:
         listoflines.append(line.strip())
 
-# converting list into string 
+# converting list into string
 
 model_history = listoflines[-1:]
-str1 = ""
+str1 = ''
 for element in model_history:
     str1 = element
-#print(str1)
 
-#converting string into float
+# print(str1)
+
+# converting string into float
+
 accuracy = str1[80:87]
-print(accuracy)
+print accuracy
 accuracy_f = float(accuracy)
 val_accuracy = str1[122:129]
-print(val_accuracy)
+print val_accuracy
 val_accuracy_f = float(val_accuracy)
 
 # saving variable into seperate files
-acc_file=open("/root/model/accuracy.txt","w")
+
+acc_file = open('/root/model/accuracy.txt', 'w')
 acc_file.write('%f' % accuracy_f)
 acc_file.close()
 
-val_acc_file=open("/root/model/val_accuracy.txt","w")
+val_acc_file = open('/root/model/val_accuracy.txt', 'w')
 val_acc_file.write('%f' % val_accuracy_f)
 val_acc_file.close()
 
