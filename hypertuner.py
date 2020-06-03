e64a6ec9098a0c5801eb1288ff3051f3481f4e20
Himani Agarwal <52791723+17ejtcs031@users.noreply.github.com>
2020-06-03 13:27:02 +0530

    h1

--- hypertuner.py
+++ hypertuner.py
@@ -3,20 +3,16 @@
 #taking the value of accuracy and val_accuracy file
 
 str1 = ""
-with open('/root/model/accuracy.txt','r') as afile:
-    for line in afile:
+af=open('/root/model/accuracy.txt','r') 
+    for line in af:
         str1 = line
+af.close()
       
 acc_in_float=float(str1)
-
 str2 = ""
-with open('/root/model/val_accuracy.txt','r') as bfile:
+vf=open('/root/model/val_accuracy.txt','r') 
     for line in bfile:
         str2 = line
-bfile.close()        
+vf.close()       
 val_acc_in_float=float(str2)     
 printf("from here doe")
-  afile.close()  
-  bfile.close()
-  
-
